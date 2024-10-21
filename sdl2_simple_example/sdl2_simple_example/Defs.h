62cf9d55ec3e90dee25ae4e48485b6a3fcde7a80
Hugo Castaño Cuéllar <hugocastanocuellar@gmail.com>
2024-10-21 14:29:43 +0200

    Funciona

--- sdl2_simple_example/sdl2_simple_example/Defs.h
+++ sdl2_simple_example/sdl2_simple_example/Defs.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef __DEFS_H__
+#define __DEFS_H__
+
+#include <GL/glew.h>
+#include <chrono>
+#include <thread>
+#include <exception>
+#include <glm/glm.hpp>
+#include <SDL2/SDL_events.h>
+#include "imgui_impl_sdl2.h"
+#include "MyWindow.h"
+#include "IL/il.h"
+#include "Windows.h"
+
+
+#endif // !__DEFS_H__
