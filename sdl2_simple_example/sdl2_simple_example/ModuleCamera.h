483662727508578af965cc433614cea6e4680729
Bernat <bernat.cifuentes@estudiantat.upc.edu>
2024-11-08 00:19:05 +0100

    Fent classes

--- sdl2_simple_example/sdl2_simple_example/ModuleCamera.h
+++ sdl2_simple_example/sdl2_simple_example/ModuleCamera.h
@@ -4,3 +4,18 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include "glm/gtx/transform.hpp"
 
+class ModuleCamera : public Module
+{
+public:
+	ModuleCamera(Application* app);
+	~ModuleCamera();
+
+	bool Start();
+	bool Update(float dt);
+	void HandleInput();
+	void FrameSelected();
+	bool CleanUp();
+
+	void LookAt(const glm::vec3& spot);
+	const glm::mat4& GetViewMatrix() const;
+	glm::mat4 GetProjectionMatrix() const;
