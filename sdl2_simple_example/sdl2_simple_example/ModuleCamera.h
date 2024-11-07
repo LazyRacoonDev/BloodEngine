240eae19c4582f1d3e27c5b29f45db070fb79e00
Bernat <bernat.cifuentes@estudiantat.upc.edu>
2024-11-08 00:35:59 +0100

    Ultimes clases

--- sdl2_simple_example/sdl2_simple_example/ModuleCamera.h
+++ sdl2_simple_example/sdl2_simple_example/ModuleCamera.h
@@ -4,6 +4,8 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include "glm/gtx/transform.hpp"
 
+/* Se tiene que hacer un modulo de input*/
+
 class ModuleCamera : public Module
 {
 public:
@@ -29,3 +31,23 @@ private:
 	glm::vec3 RotateVector(glm::vec3 const& vector, float angle, glm::vec3 const& axis);
 
 	void SetCursor(CursorType cursorType);
+
+public:
+	float fov = 60.0f;
+	float nearPlane = 0.125f;
+	float farPlane = 512.0f;
+	int screenWidth, screenHeight;
+
+	bool isMouseInside = false;
+
+private:
+	glm::vec3 X, Y, Z;
+	glm::vec3 pos, ref;
+	glm::mat4 viewMatrix;
+
+	bool isZooming = false;
+	bool isOrbiting = false;
+	bool isFreeLook = false;
+	bool isDragging = false;
+	bool isDefaultCursor = true;
+};
