17cf440451bcdcba01af18bbd76e30abe436dce3
Bernat <bernat.cifuentes@estudiantat.upc.edu>
2024-11-08 00:27:17 +0100

    Mes classes

--- sdl2_simple_example/sdl2_simple_example/ModuleCamera.h
+++ sdl2_simple_example/sdl2_simple_example/ModuleCamera.h
@@ -19,3 +19,13 @@ public:
 	void LookAt(const glm::vec3& spot);
 	const glm::mat4& GetViewMatrix() const;
 	glm::mat4 GetProjectionMatrix() const;
+
+private:
+	void HandleMovement(glm::vec3& newPos, float speed, float fastSpeed);
+	void HandleZoom(float zoomSpeed);
+	void HandleRotation();
+	void RotateCamera(int dx, int dy);
+	void CalculateViewMatrix();
+	glm::vec3 RotateVector(glm::vec3 const& vector, float angle, glm::vec3 const& axis);
+
+	void SetCursor(CursorType cursorType);
