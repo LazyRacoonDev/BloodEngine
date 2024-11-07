91e66a34faca41f1b02abec81fdac6e13a373762
Bernat <bernat.cifuentes@estudiantat.upc.edu>
2024-11-08 00:45:03 +0100

    Mes cosetes

--- sdl2_simple_example/sdl2_simple_example/ModuleCamera.cpp
+++ sdl2_simple_example/sdl2_simple_example/ModuleCamera.cpp
@@ -1,2 +1,17 @@
 #include "ModuleCamera.h"
 #include "Application.h"
+
+ModuleCamera::ModuleCamera(Application* app) : Module(app)
+{
+	screenWidth = app->window->width;
+	screenHeight = app->window->height;
+
+	CalculateViewMatrix();
+
+	X = glm::vec3(1.0f, 0.0f, 0.0f);
+	Y = glm::vec3(0.0f, 1.0f, 0.0f);
+	Z = glm::vec3(0.0f, 0.0f, 1.0f);
+
+	pos = glm::vec3(0.0f, 5.0f, 5.0f);
+	ref = glm::vec3(0.0f, 0.0f, 0.0f);
+}
